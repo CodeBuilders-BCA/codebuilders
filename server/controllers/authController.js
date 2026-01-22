a0059e62c4714422eb4cf902baf97cf8d91bd4c1
Abdus Samad <abdussamadshamsi486@gmail.com>
2026-01-22 13:05:54 +0530

    lax removed

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -105,8 +105,8 @@ const logout = (req, res) => {
   res.cookie('token', '', {
     httpOnly: true,
     expires: new Date(0),
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    secure: process.env.NODE_ENV === 'production,
+    sameSite: 'none',
   });
   res.status(200).json({ message: 'Logged out successfully' });
 };
